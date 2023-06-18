3fd50ed9b225840999c79a10f877004b910e3f5c
Sam <dacosta.peon@gmail.com>
2023-06-18 22:10:52 +0200

    Add files and basic arithmetic functions

---
+++ myScript.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    total = num1 + num2;
+    console.log(total)
+}
+
+function subs(num1, num2) {
+    total = num1 - num2;
+    console.log(total)
+}
+
+function mult(num1, num2) {
+    total = num1 * num2;
+    console.log(total)
+}
+
+function div(num1, num2) {
+    total = num1 / num2;
+    console.log(total)
+}
