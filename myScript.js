de5a14136adca4f28a660698db41fba6a8d90490
Sam <dacosta.peon@gmail.com>
2023-06-24 21:06:45 +0200

    can do several pairs of operations

--- myScript.js
+++ myScript.js
@@ -8,44 +8,115 @@ let secDis = document.getElementById("secDisplay");
 let buttons = document.querySelectorAll("button");
 
 for (button of buttons) {
+
     button.addEventListener("click", function(e) {
             
-            if (e.target.id === "clear") {
+        if (e.target.id === "clear") {
+
             firsDis.textContent = "";
             secDis.textContent = "";
+            firNum = undefined;
+            secNum = undefined;
+            oper = undefined;
+            total = undefined;
+
         } else if (e.target.className === "num") {
+
             firsDis.textContent += e.target.id;
+
         } else if (e.target.id === "add") {
-            firNum = document.getElementById("priDisplay").textContent;
-            secDis.textContent = firNum + "+";
-            oper = "+"
-            firsDis.textContent = "";            
+
+            if (secDis.textContent !== "") {
+
+                secNum = document.getElementById("priDisplay").textContent;
+                total = operate(firNum, oper, secNum);
+                secDis.textContent = total + "+";
+                firNum = total;
+                oper = "+";
+                firsDis.textContent = "";
+
+            } else {
+
+                firNum = document.getElementById("priDisplay").textContent;
+                secDis.textContent = firNum + "+";
+                oper = "+"
+                firsDis.textContent = "";
+
+            }
+
+            
         } else if (e.target.id === "subs") {
-            firNum = document.getElementById("priDisplay").textContent;
-            firsDis.textContent = "";
-            oper = "-"
-            secDis.textContent = firNum + "-";
-            firsDis.textContent = "";
-            console.log(firNum)
+
+            if (secDis.textContent !== "") {
+
+                secNum = document.getElementById("priDisplay").textContent;
+                total = operate(firNum, oper, secNum);
+                secDis.textContent = total + "-";
+                firNum = total;
+                oper = "-";
+                firsDis.textContent = "";
+
+            } else {
+
+                firNum = document.getElementById("priDisplay").textContent;
+                firsDis.textContent = "";
+                oper = "-"
+                secDis.textContent = firNum + "-";
+                firsDis.textContent = "";
+                
+            }
+
         } else if (e.target.id === "mult") {
-            firNum = document.getElementById("priDisplay").textContent;
-            firsDis.textContent = "";
-            oper = "*"
-            secDis.textContent = firNum + "*";
-            firsDis.textContent = "";
-            console.log(firNum)
+            if (secDis.textContent !== "") {
+
+                secNum = document.getElementById("priDisplay").textContent;
+                total = operate(firNum, oper, secNum);
+                secDis.textContent = total + "*";
+                firNum = total;
+                oper = "*";
+                firsDis.textContent = "";
+
+            } else {
+
+                firNum = document.getElementById("priDisplay").textContent;
+                firsDis.textContent = "";
+                oper = "*"
+                secDis.textContent = firNum + "*";
+                firsDis.textContent = "";
+
+            }
+
         } else if (e.target.id === "div") {
-            firNum = document.getElementById("priDisplay").textContent;
-            oper = "/"
-            firsDis.textContent = "";
-            secDis.textContent = firNum + "/";
-            firsDis.textContent = "";
-            console.log(firNum)
+
+            if (secDis.textContent !== "") {
+
+                secNum = document.getElementById("priDisplay").textContent;
+                total = operate(firNum, oper, secNum);
+                secDis.textContent = total + "/";
+                firNum = total;
+                oper = "/";
+                firsDis.textContent = "";
+
+            } else {
+
+                firNum = document.getElementById("priDisplay").textContent;
+                oper = "/"
+                firsDis.textContent = "";
+                secDis.textContent = firNum + "/";
+                firsDis.textContent = "";
+
+            }
+
         } else if (e.target.id === "equal") {
-            secNum = document.getElementById("priDisplay").textContent;
-            total = operate(firNum, oper, secNum);
-            secDis.textContent += secNum;
-            firsDis.textContent = total;
+
+            if (secDis.textContent !== "") {
+
+                secNum = document.getElementById("priDisplay").textContent;
+                total = operate(firNum, oper, secNum);
+                secDis.textContent += secNum;
+                firsDis.textContent = total;
+
+            }
         }
         
     })
@@ -71,25 +142,23 @@ function add(num1, num2) {
     num1 = Number(num1);
     num2 = Number(num2);
     total = num1 + num2;
-    console.log(total)
     return total;
 }
 
 function subs(num1, num2) {
     total = num1 - num2;
-    console.log(total)
+    console.log(typeof total)
     return total;
+
 }
 
 function mult(num1, num2) {
     total = num1 * num2;
-    console.log(total)
     return total;
 }
 
 function div(num1, num2) {
     total = num1 / num2;
-    console.log(total)
     return total;
 }
 
